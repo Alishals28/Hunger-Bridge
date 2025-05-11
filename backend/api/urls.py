5b6038edaeb3cc1ad38d91f3edaffe7f42d4c414
Alisha Siddiqui <147940081+Alishals28@users.noreply.github.com>
2025-05-11 16:42:00 +0500

    Notification system

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, DonationViewSet, NGOViewSet, VolunteerViewSet, RequestViewSet, TransactionViewSet, RouteViewSet
+from .views import UserViewSet, DonationViewSet, NGOViewSet, VolunteerViewSet, RequestViewSet, TransactionViewSet, RouteViewSet, test_notification, NotificationViewSet
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from .views import test_mongo_connection
 
 # Create a router and register viewsets
 router = DefaultRouter()
@@ -15,6 +16,7 @@ router.register(r'volunteers', VolunteerViewSet)
 router.register(r'requests', RequestViewSet)
 router.register(r'transactions', TransactionViewSet)
 router.register(r'routes', RouteViewSet)
+router.register(r'notifications2', NotificationViewSet, basename='notifications')
 
 urlpatterns = [
     path('', include(router.urls)),
@@ -24,4 +26,7 @@ urlpatterns = [
 urlpatterns += [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('test-notification/', test_notification, name='test_notification'),
+    path('test-mongo/', test_mongo_connection, name='test_mongo_connection'),
+
 ]
