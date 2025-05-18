5da6247d4d6f03711ada2c77985f47cfc5460606
Rimsha Nosheen <mishihwanghoo@gmail.com>
2025-05-18 19:56:43 +0500

    update

--- backend/api/views.py
+++ backend/api/views.py
@@ -569,7 +569,9 @@ class CustomTokenObtainPairView(TokenObtainPairView):
 class RegisterView(APIView):
     permission_classes = [AllowAny]
     def post(self, request):
+
         serializer = RegisterSerializer(data=request.data)
+        permission_classes=[AllowAny]
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
